9f7c6f325ed73e4867eada9677fc2e0618656aca
vkaraahmet <vkaraahmet@hepsiemlak.com>
2023-09-21 14:05:07 +0300

    add order

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000; // or any port you prefer
+
+var cors = require('cors')
+app.use(cors())
+
+app.use(bodyParser.json());
+app.post('/api/order', (req, res) => {
+  const formData = req.body;
+  console.log('Received form data:', formData);
+  res.status(200).json({ message: 'Order submitted successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
